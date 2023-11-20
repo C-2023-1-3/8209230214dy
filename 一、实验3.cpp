a4d2bfc447b69ec16f673f2f7ef057227c7d3670
unknown <1303849129@qq.com>
2023-11-20 13:08:18 +0800

    2023 1120 1308

--- 一、实验3.cpp
+++ 一、实验3.cpp
@@ -1,9 +1,12 @@
-﻿#include<iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
 	cout << "char length:" << sizeof(char) << endl;
 	cout << "int length:" << sizeof(int) << endl;
+	cout << "short length:" << sizeof(short) << endl;
+	cout << "long length:" << sizeof(long) << endl;
+	cout << "long long length:" << sizeof(long long) << endl;
+	cout << "string length:" << sizeof(string) << endl;
 	return 0;
 }
-
